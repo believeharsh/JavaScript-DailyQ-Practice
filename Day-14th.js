09e4e97a7d9fa81b7d7e6851775c77a04aee928a
Bontinue <93024harsh@gmail.com>
2023-04-18 22:26:23 +0530

    second commit of the 14th day

--- Day-14th.js
+++ Day-14th.js
@@ -41,3 +41,24 @@ function Arrays_sum(array1, array2)
 
 console.log(Arrays_sum([1,0,2,3,4], [3,5,6,7,8,13]));
 
+
+// 20. Write a JavaScript program to find duplicate values in a JavaScript array.
+function find_duplicate_in_array(arra1) {
+    var object = {};
+    var result = [];
+
+    arra1.forEach(function (item) {
+      if(!object[item])
+          object[item] = 0;
+        object[item] += 1;
+    })
+
+    for (var prop in object) {
+       if(object[prop] >= 2) {
+           result.push(prop);
+       }
+    }
+
+    return result;
+
+}
