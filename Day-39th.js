8e1d95ea5f62f4460c38e9017b97c71648b5517f
Bontinue <93024harsh@gmail.com>
2023-06-15 17:06:53 +0530

    file has been added on day-40.html

---
+++ Day-39th.js
@@ -0,0 +1,74 @@
+// JavaScript / ECMAScript
+// JavaScript was invented by Brendan Eich in 1995.
+
+// It was developed for Netscape 2, and became the ECMA-262 standard in 1997.
+
+// After Netscape handed JavaScript over to ECMA, the Mozilla foundation continued to develop JavaScript for the Firefox browser. Mozilla's latest version was 1.8.5. (Identical to ES5).
+
+// Internet Explorer (IE4) was the first browser to support ECMA-262 Edition 1 (ES1).
+
+// Year	ECMA	Browser
+// 1995	 	JavaScript was invented by Brendan Eich
+// 1996	 	Netscape 2 was released with JavaScript 1.0
+// 1997	 	JavaScript became an ECMA standard (ECMA-262)
+// 1997	ES1	ECMAScript 1 was released
+// 1997	ES1	IE 4 was the first browser to support ES1
+// 1998	ES2	ECMAScript 2 was released
+// 1998	 	Netscape 42 was released with JavaScript 1.3
+// 1999	ES2	IE 5 was the first browser to support ES2
+// 1999	ES3	ECMAScript 3 was released
+// 2000	ES3	IE 5.5 was the first browser to support ES3
+// 2000	 	Netscape 62 was released with JavaScript 1.5
+// 2000	 	Firefox 1 was released with JavaScript 1.5
+// 2008	ES4	ECMAScript 4 was abandoned
+// 2009	ES5	ECMAScript 5 was released
+// 2011	ES5	IE 9 was the first browser to support ES5 *
+// 2011	ES5	Firefox 4 was released with JavaScript 1.8.5
+// 2012	ES5	Full support for ES5 in Safari 6
+// 2012	ES5	Full support for ES5 in IE 10
+// 2012	ES5	Full support for ES5 in Chrome 23
+// 2013	ES5	Full support for ES5 in Firefox 21
+// 2013	ES5	Full support for ES5 in Opera 15
+// 2014	ES5	Full support for ES5 in all browsers
+// 2015	ES6	ECMAScript 6 was released
+// 2016	ES6	Full support for ES6 in Chrome 51
+// 2016	ES6	Full support for ES6 in Opera 38
+// 2016	ES6	Full support for ES6 in Safari 10
+// 2017	ES6	Full support for ES6 in Firefox 54
+// 2017	ES6	Full support for ES6 in Edge 15
+// 2018	ES6	Full support for ES6 in all browsers **
+// Note
+// * Internet Explorer 9 did not support ES5 "use strict".
+
+// ** Internet Explorer 11 does not support ES6.
+
+// The ECMA Technical Committee 39
+// In 1996, Netscape and Brendan Eich took JavaScript to the ECMA international standards organization, and a technical committee (TC39) was created to develop the language.
+
+// ECMA-262 Edition 1 was released in June 1997.
+
+// ADVERTISEMENT
+
+// From ES4 to ES6
+// When the TC39 committee got together in Oslo in 2008, to agree on ECMAScript 4, they were divided into 2 very different camps:
+
+// The ECMAScript 3.1 Camp:
+// Microsoft and Yahoo who wanted an incremental upgrade from ES3.
+
+// The ECMAScript 4 Camp:
+// Adobe, Mozilla, Opera, and Google who wanted a massive ES4 upgrade.
+
+// August 13 2008, Brendan Eich wrote an email:
+
+// It's no secret that the JavaScript standards body, Ecma's Technical Committee 39, has been split for over a year, with some members favoring ES4, a major fourth edition to ECMA-262, and others advocating ES3.1 based on the existing ECMA-262 Edition 3 (ES3) specification. Now, I'm happy to report, the split is over.
+
+// The solution was to work together:
+
+// ECMAScript 4 was renamed to ES5
+// ES5 should be an incremental upgrade of ECMAScript 3.
+// Features of ECMAScript 4 should be picked up in later versions.
+// TC39 should develop a new major release, bigger in scope than ES5.
+// The planned new release (ES6) was codenamed "Harmony" (Because of the split it created?).
+
+// ES5 was a huge success. It was released in 2009, and all major browsers (including Internet Explorer) were fully compliant by July 2013:
+
