234cda68a6e96eeac131cb66b6b238020cfc3a23
Bontinue <93024harsh@gmail.com>
2023-07-27 19:26:33 +0530

    added 55-pyramid-of-doom.js

---
+++ 55-pyramid-of-doom.js
@@ -0,0 +1,36 @@
+// 1. without error handling
+function loadscript(src, callback) {
+    let script = document.createElement('script');
+    script.src = src;
+    script.onload = function () {
+        console.log("script loaded : " + src)
+        callback(src)
+        function loadscript(src, callback1) {
+            let script = document.createElement('script');
+            script.src = src;
+            script.onload = function () {
+                console.log("script loaded : " + src)
+                callback1(src)
+                function loadscript(src, callback3) {
+                    let script = document.createElement('script');
+                    script.src = src;
+                    script.onload = function () {
+                        console.log("script loaded : " + src)
+                        callback3(src)
+
+
+                    }
+
+                    document.body.appendChild(script);
+                }
+
+                function goodmorning(src) {
+
+                    alert("hello seocnd brohter")
+                    console.log("goodmorning" + src);
+                }
+                loadscript("https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js", goodmorning)
+            }
+        }
+    }
+}
