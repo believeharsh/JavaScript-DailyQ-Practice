b5baa8e80433f25eb89c7f93215a0813762b35b3
Bontinue <93024harsh@gmail.com>
2023-07-31 23:21:20 +0530

    added 56 promise.js

---
+++ 56-Promise.js
@@ -0,0 +1,11 @@
+let promise = new Promise(function(resolve, reject){
+  alert("hello bhai mehnat kerle werna pachtayega ");
+  resolve(45);
+})
+
+console.log("hello one")
+setTimeout(function(){
+alert("hello 2 in two seconds");
+},2000)
+console.log("my name is : Harsh three");
+console.log(promise)
