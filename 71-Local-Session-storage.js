669193312146d1f0ce0c3551dc5d3b0c635dab21
Believeharsh <93024harsh@gmail.com>
2023-08-14 14:26:05 +0530

    added session storage concept

--- 71-Local-Session-storage.js
+++ 71-Local-Session-storage.js
@@ -27,3 +27,18 @@ if (key == 0) {
 // localStorage.removeItem('sdfsdfksadfj')
 // localStorage.removeItem("Name")
 // localStorage.clear()
+
+
+
+// /What is Session Storage?
+
+// Session Storage objects can be accessed using the sessionStorage read-only property. The difference between sessionStorage and localStorage is that localStorage data does not expire, whereas sessionStorage data is cleared when the page session ends
+
+// Local : The storage capacity of local storage is 5MB/10MB
+// Session : The storage capacity of session storage is 5MB
+// Cookie : The storage capacity of Cookies is 4KB
+
+window.onstorage = (e) => {
+    alert("changed")
+    console.log(e)
+  }
