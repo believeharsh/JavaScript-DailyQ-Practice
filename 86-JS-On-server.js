88469970dd29d6150ad4837a4bee520acce8aed0
Believeharsh <93024harsh@gmail.com>
2023-08-29 20:17:41 +0530

    added 86-JS-On-server.js

---
+++ 86-JS-On-server.js
@@ -0,0 +1,19 @@
+// what is repl : A Read-Eval-Print Loop, or REPL, is a computer environment where user inputs are read and evaluated, and then the results are returned to the user. REPLs provide an interactive environment to explore tools available in specific environments or programming languages. Some examples include the Node.js console, IPython, the Bash shell, and the developer console found in most web browsers.
+
+let JsOntheserver = "Hii you can also use Js on the server with the help of the NodeJs enviroment";
+console.log(JsOntheserver);
+
+
+// History of Node Js Enviorment in the coding Industery
+// What Can Node.js Do?
+// Node.js can generate dynamic page content
+// Node.js can create, open, read, write, delete, and close files on the server
+// Node.js can collect form data
+// Node.js can add, delete, modify data in your database
+
+
+// What is a Node.js File?
+// Node.js files contain tasks that will be executed on certain events
+// A typical event is someone trying to access a port on the server
+// Node.js files must be initiated on the server before having any effect
+// Node.js files have extension ".js"
