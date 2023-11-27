a0e25c1a7dc5ce2929ad5ef97cb807d94118ae3e
Believeharsh <bontinue@gmail.com>
2023-11-27 16:59:52 +0530

    formated

--- 98-Map-Mathod-detail.js
+++ 98-Map-Mathod-detail.js
@@ -8,18 +8,17 @@
 // syntax
 // array.map(function(currentValue, index, arr), thisValue)
 
-
 const persons = [
-    {firstname : "Malcom", lastname: "Reynolds"},
-    {firstname : "Kaylee", lastname: "Frye"},
-    {firstname : "Jayne", lastname: "Cobb"}
-  ];
-  
-   const HD = persons.map(getFullName);
-  
-  function getFullName(item) {
-    return [item.firstname, item.lastname].join(" ");
-  }
-console.log(HD)
+  { firstname: "Malcom", lastname: "Reynolds" },
+  { firstname: "Kaylee", lastname: "Frye" },
+  { firstname: "Jayne", lastname: "Cobb" },
+];
+
+const HD = persons.map(getFullName);
+
+function getFullName(item) {
+  return [item.firstname, item.lastname].join(" ");
+}
+console.log(HD);
 //
 // Yes, in JavaScript, the map method always returns a new array. The map method is used to create a new array by applying a provided function to each element of the original array. The function is applied to every element in the array, and the results are collected into a new array. It does not modify the original array.
