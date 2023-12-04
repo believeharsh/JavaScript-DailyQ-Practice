d8aea729bbd81840d101495624de079da9d86ea4
Believeharsh <bontinue@gmail.com>
2023-12-04 10:59:23 +0530

    formated

--- 100-Strict-Mode.js
+++ 100-Strict-Mode.js
@@ -1,8 +1,14 @@
 
 // Strict mode in JavaScript is a feature introduced in ECMAScript 5 (ES5) that allows developers to write more reliable and maintainable code by catching common mistakes and preventing the use of certain error-prone features. When strict mode is enabled, the JavaScript interpreter is more strict in parsing and executing code, which helps in identifying and fixing potential issues early in the development process.
 
-"use strict"
+// "use strict"
 harsh = 10;
 console.log(harsh)
 let num1 = 10; 
 console.log(num1)
+
+
+var obj = {};
+Object.defineProperty(obj, "x", { value: 10, writable: false });
+obj.x = 20;
+console.log(obj.x)
