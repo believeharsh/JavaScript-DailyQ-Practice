d189d295fa7d28d630348687aee8d2228671f788
Believeharsh <bontinue@gmail.com>
2023-12-19 15:41:07 +0530

    Revise .then and .catch methods succussfully

--- 104-PromiseInDetail.js
+++ 104-PromiseInDetail.js
@@ -1,4 +1,4 @@
-// What is Promise in JavaScript ? 
+// What is Promise in JavaScript ?
 
 // In JavaScript, a promise is an object that represents the eventual completion or failure of an asynchronous operation and its resulting value. Promises are used to handle asynchronous operations in a more manageable and readable way compared to traditional callback-based approaches.
 
@@ -10,27 +10,68 @@
 
 // Rejected: The operation failed, and the promise has a reason for the failure.
 
-
 const myPromise = new Promise((resolve, reject) => {
     // Simulating an asynchronous operation (e.g., fetching data)
     setTimeout(() => {
-      const success = false;
-  
-      if (success) {
-        resolve("Operation successful");
-      } else {
-        reject("Operation failed");
-      }
-    }, 1000);
-  });
-  
-  // Using the promise
-  myPromise
-    .then((result) => {
-      console.log(result); // Output: const succuss = ture ? Operation successful : Operation failed
-    })
-  .catch((result)=>{
-    console.log(result)
-  })
+        const success = false;
 
-//   The then method is used to handle the successful completion of the promise, and the catch method is used to handle any errors or rejections. Promises provide a more structured way to work with asynchronous code and avoid callback hell, making it easier to reason about and maintain asynchronous code.
+        if (success) {
+            resolve("Operation successful");
+        } else {
+            reject("Operation failed");
+        }
+    }, 1000);
+});
+
+// Using the promise
+myPromise
+    .then((result) => {
+        console.log(result); // Output: const succuss = ture ? Operation successful : Operation failed
+    })
+    .catch((result) => {
+        console.log(result);
+    });
+
+    //   The then method is used to handle the successful completion of the promise, and the catch method is used to handle any errors or rejections. Promises provide a more structured way to work with asynchronous code and avoid callback hell, making it easier to reason about and maintain asynchronous code.
+
+    // 1.Promise then() Method: It is invoked when a promise is either resolved or rejected. It may also be defined as a carrier that takes data from promise and further executes it successfully.
+
+    // Parameters: It takes two functions as parameters.
+
+    // The first function is executed if the promise is resolved and a result is received.
+    // The second function is executed if the promise is rejected and an error is received. (It is optional and there is a better way to handle error using .catch() method
+
+let promise = new Promise(function (resolve, reject) {
+    reject("Promise Rejected");
+});
+
+promise.then(
+    function (successMessage) {
+        console.log(successMessage);
+    },
+    function (errorMessage) {
+        //error handler function is invoked
+        console.log(errorMessage);
+    }
+);
+
+    //2. Promise catch() Method: It is invoked when a promise is either rejected or some error     has occurred in execution. It is used as an Error Handler whenever at any step there is a chance of getting an error.
+
+    // Parameters: It takes one function as a parameter.
+
+    // Function to handle errors or promise rejections.(.catch() method internally calls .then(null, errorHandler), i.e. .catch() is just a shorthand for .then(null, errorHandler) )
+
+
+
+let promise2 = new Promise(function (resolve, reject) {
+    reject('Promise Rejected')
+})
+ 
+promise2
+    .then(function (successMessage) {
+        console.log(successMessage);
+    })
+    .catch(function (errorMessage) {
+        //error handler function is invoked 
+        console.log(errorMessage);
+    });
