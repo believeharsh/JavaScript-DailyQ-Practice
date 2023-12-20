7e8a9529c883dc4414fa7f710ca84f67cdcf702d
Believeharsh <bontinue@gmail.com>
2023-12-20 16:07:20 +0530

    formated

--- 104-PromiseInDetail.js
+++ 104-PromiseInDetail.js
@@ -32,14 +32,14 @@ myPromise
         console.log(result);
     });
 
-    //   The then method is used to handle the successful completion of the promise, and the catch method is used to handle any errors or rejections. Promises provide a more structured way to work with asynchronous code and avoid callback hell, making it easier to reason about and maintain asynchronous code.
+//   The then method is used to handle the successful completion of the promise, and the catch method is used to handle any errors or rejections. Promises provide a more structured way to work with asynchronous code and avoid callback hell, making it easier to reason about and maintain asynchronous code.
 
-    // 1.Promise then() Method: It is invoked when a promise is either resolved or rejected. It may also be defined as a carrier that takes data from promise and further executes it successfully.
+// 1.Promise then() Method: It is invoked when a promise is either resolved or rejected. It may also be defined as a carrier that takes data from promise and further executes it successfully.
 
-    // Parameters: It takes two functions as parameters.
+// Parameters: It takes two functions as parameters.
 
-    // The first function is executed if the promise is resolved and a result is received.
-    // The second function is executed if the promise is rejected and an error is received. (It is optional and there is a better way to handle error using .catch() method
+// The first function is executed if the promise is resolved and a result is received.
+// The second function is executed if the promise is rejected and an error is received. (It is optional and there is a better way to handle error using .catch() method
 
 let promise = new Promise(function (resolve, reject) {
     reject("Promise Rejected");
@@ -55,18 +55,18 @@ promise.then(
     }
 );
 
-    //2. Promise catch() Method: It is invoked when a promise is either rejected or some error     has occurred in execution. It is used as an Error Handler whenever at any step there is a chance of getting an error.
+//2. Promise catch() Method: It is invoked when a promise is either rejected or some error     has occurred in execution. It is used as an Error Handler whenever at any step there is a chance of getting an error.
 
-    // Parameters: It takes one function as a parameter.
+// Parameters: It takes one function as a parameter.
 
-    // Function to handle errors or promise rejections.(.catch() method internally calls .then(null, errorHandler), i.e. .catch() is just a shorthand for .then(null, errorHandler) )
+// Function to handle errors or promise rejections.(.catch() method internally calls .then(null, errorHandler), i.e. .catch() is just a shorthand for .then(null, errorHandler) )
 
 
 
 let promise2 = new Promise(function (resolve, reject) {
     reject('Promise Rejected')
 })
- 
+
 promise2
     .then(function (successMessage) {
         console.log(successMessage);
@@ -77,14 +77,23 @@ promise2
     });
 
 
-    // ****** Example with fetch api function of JS
+// ****** Example with fetch api function of JS
 
-    let fetchRes = fetch(
-        "https://jsonplaceholder.typicode.com/todos/1");
-        
-        // FetchRes is the promise to resolve
-        // it by using.then() method
-        fetchRes.then(res =>
-            res.json()).then(d => {
-                console.log(d)
-            })
+let fetchRes = fetch(
+    "https://jsonplaceholder.typicode.com/todos/1");
+
+// FetchRes is the promise to resolve
+// it by using.then() method
+fetchRes.then(res =>
+    res.json()).then(d => {
+        console.log(d)
+    })
+
+
+// ********** another Example
+
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+delay(1000)
+    .then(() => console.log('Delayed operation complete'))
+    .catch(error => console.error('Error:', error));
