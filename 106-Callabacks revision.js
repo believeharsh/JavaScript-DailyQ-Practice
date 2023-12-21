e3a75235dff96882419f2a71eb426e312901635b
Believeharsh <bontinue@gmail.com>
2023-12-21 11:34:54 +0530

    added 106-Callbacks revision.js

---
+++ 106-Callabacks revision.js
@@ -0,0 +1,13 @@
+const cart = ['Macbook', 'Mointar', 'I phone'];
+
+Api.createOrder(cart, function(){
+    Api1.Proceedtopayment(function(){
+        Api2.ShowOrderSummery(function(){
+            Api3.UpdateWallet(function(){
+                Api4.AlertOrderEmail()
+            })
+        })
+    })
+})
+
+// The above function is the perfect example of callbacks hells where our each api is dependent on other api so that if a single error will ocur then entire function will collapse
