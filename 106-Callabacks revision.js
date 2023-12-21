076ed01c94446d4d06f3e9e7e9cefd2f787fbbc5
Believeharsh <bontinue@gmail.com>
2023-12-21 20:43:47 +0530

    added 106-callbacks revision.js

--- 106-Callabacks revision.js
+++ 106-Callabacks revision.js
@@ -2,7 +2,7 @@ const cart = ['Macbook', 'Mointar', 'I phone'];
 
 Api.createOrder(cart, function(){
     Api1.Proceedtopayment(function(){
-        Api2.ShowOrderSummery(function(){
+        Api2.ShowOrderSummery(function(){ /// this is ca
             Api3.UpdateWallet(function(){
                 Api4.AlertOrderEmail()
             })
@@ -11,3 +11,6 @@ Api.createOrder(cart, function(){
 })
 
 // The above function is the perfect example of callbacks hells where our each api is dependent on other api so that if a single error will ocur then entire function will collapse
+
+
+// inversion of Control : 
