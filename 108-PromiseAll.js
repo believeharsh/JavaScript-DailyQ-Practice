81cb19c63f0d80f421b0690e48cbc2353aef291b
Believeharsh <bontinue@gmail.com>
2023-12-23 12:25:47 +0530

    added promise.all() definition

--- 108-PromiseAll.js
+++ 108-PromiseAll.js
@@ -1,4 +1,6 @@
-
+// The Promise.all() static method takes an iterable of promises as input and returns a single Promise. 
+// This returned promise fulfills when all of the input's promises fulfill (including when an empty iterable is passed), with an array of the fulfillment values. It rejects when any of the input's promises rejects, with this first rejection reason. 
+// the promise.all() method always return an Array which contain all the value of resolved promises. 
 
 
 
