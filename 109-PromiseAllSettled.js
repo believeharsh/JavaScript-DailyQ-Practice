9c1ff3e6cea0ddffedb8cc6e17eff2bfc7270361
Believeharsh <bontinue@gmail.com>
2023-12-23 19:15:57 +0530

    added defintion

--- 109-PromiseAllSettled.js
+++ 109-PromiseAllSettled.js
@@ -1,16 +1,16 @@
 /// Promise.AllSettled()
 
-
+// The Promise.allSettled() method in JavaScript is used to get a promise when all inputs are settled that is either fulfilled or rejected.
 
 const p1 = new Promise((res,rej) => {
-  setTimeout(()=> res("P1 has been succussful"),1000);
+  setTimeout(()=> res("P1 has been succussful"),3000);
 });
 const p2 = new Promise((res,rej) => {
   setTimeout(()=> res("P2 has been succussful"),2000);
 });
 const p3 = new Promise((res, rej) => {
-  setTimeout(()=> rej("P3 has been succussful"),5000);
-  setTimeout(()=> res("P3 has been succussful"),5000);
+  setTimeout(()=> rej("P3 has been succussful"),1000);
+//   setTimeout(()=> res("P3 has been succussful"),5000);
 });
 
 Promise.allSettled([p1,p2,p3])
