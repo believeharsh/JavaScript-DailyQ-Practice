c82bba05c62146532e8dee18642ce438616c9252
Believeharsh <bontinue@gmail.com>
2023-12-29 15:27:30 +0530

    added real world example with good understanding of Async Await and try catch

--- 116-Async-Await.js
+++ 116-Async-Await.js
@@ -5,40 +5,51 @@
 // Async functions always return a promise. If the return value of an async function is not explicitly a promise, it will be implicitly wrapped in a promise.
 
 const p = new Promise((res, rej) => {
-    setTimeout(() => {
-      res ("Promise is resolved now !!")
-    },10000)
-    
-  })
-  const p2 = new Promise((res, rej) => {
-    setTimeout(() => {
-      res ("Promise2 is resolved now !!")
-    })
-    
-  })
-  
-  const getdata = async function(){
-    console.log("pre console of promise1")
-    let val1 = await p;
-    // line 13 will wait until val1 gets the value of promise p to be resolved and reject
-    // await can only be used inside in async function only 
-    console.log("JavaScript Practice ")
-    console.log(val1)
-    console.log("pre console of promise2")
-    
-    let val2 = await p2; // Now this line again wait for 5 minutes
-    console.log("JavaScript Practice ")
-    console.log(val2)
+  setTimeout(() => {
+    res("Promise is resolved now !!");
+  }, 10000);
+});
+const p2 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("Promise2 is resolved now !!");
+  });
+});
+
+const getdata = async function () {
+  console.log("pre console of promise1");
+  let val1 = await p;
+  // line 13 will wait until val1 gets the value of promise p to be resolved and reject
+  // await can only be used inside in async function only
+  console.log("JavaScript Practice ");
+  console.log(val1);
+  console.log("pre console of promise2");
+
+  let val2 = await p2; // Now this line again wait for 5 minutes
+  console.log("JavaScript Practice ");
+  console.log(val2);
+};
+
+// If wrap up the promises with setTimeout in one particular funciton then all the promises will be resolved in the maximum time of promise.
+getdata();
+// let Resofgetdata = getdata();
+// console.log(Resofgetdata)
+
+// Resofgetdata.then((res) => console.log(res));
+
+// Let's use the async and await together :
+
+// Real world example of fetch funciton in javaScript :
+
+const apiurl = "https://api.github.com/users/believeharsh";
+async function Handledata() {
+  try {
+    const data = await fetch(apiurl);
+    const jsonvalue = await data.json();
+    console.log(jsonvalue);
+  } catch (err) {
+    console.log(err);
   }
-  
-  
-  // If wrap up the promises with setTimeout in one particular funciton then all the promises will be resolved in the maximum time of promise. 
-  getdata()
-  // let Resofgetdata = getdata();
-  // console.log(Resofgetdata)
-  
-  // Resofgetdata.then((res) => console.log(res));
-  
-  
-  // Let's use the async and await together : 
-  
+  // data.then((res) => res.json()).then((res) => console.log(res))
+  // This will give an error which is : data.then() is not a funciton
+}
+Handledata();
