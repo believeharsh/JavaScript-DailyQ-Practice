c5bcd0d2d38a6dfded60864f4205115665586058
Believeharsh <bontinue@gmail.com>
2023-12-31 16:18:46 +0530

    formated

--- 118-CallStack.js
+++ 118-CallStack.js
@@ -2,7 +2,6 @@
 
 // A call stack is a mechanism for an interpreter (like the JavaScript interpreter in a web browser) to keep track of its place in a script that calls multiple functions — what function is currently being run and what functions are called from within that function, etc.
 
-
 // When a script calls a function, the interpreter adds it to the call stack and then starts carrying out the function.
 
 // Any functions that are called by that function are added to the call stack further up, and run where their calls are reached.
@@ -11,18 +10,29 @@
 
 // If the stack takes up more space than it was assigned, a "stack overflow" error is thrown.
 
-// example for understanding the callstack 
-function one(){
-    console.log("first")
-    two()
-  }
-  function two(){
-    console.log("second")
-    three()
-  }
-  function three(){
-    console.log("third")
-  }
-  one()
-  two()
-  three()
+// example for understanding the callstack
+function one() {
+  console.log("first");
+  two();
+}
+function two() {
+  console.log("second");
+  three();
+}
+function three() {
+  console.log("third");
+}
+one(); // here the call stack will look like this
+//    |
+//    | three =>
+//    | two =>
+//    | one =>
+
+two(); // here the call stack will look like this
+//    |
+//    | three =>
+//    | two =>
+
+three(); // here the call stack will look like this
+//    |
+//    | three =>
