5333309a6e1430d403d58f8758329cdec916c533
Believeharsh <bontinue@gmail.com>
2023-12-31 09:30:14 +0530

    added 117-Undefined-NotDefined.js

---
+++ 117-Undefined-NotDefined.js
@@ -0,0 +1,15 @@
+// 1. What is undefined in js ?
+// 2. What is the mean of term called Not defined ?
+
+// console.log(a)
+var a;
+console.log(a)
+a = 10; 
+
+if(a === undefined){
+  console.log("a is undefined yet")
+}
+else{console.log("a is defined ")}
+
+
+// JavaScript knows as a loosly types languages because it don't restrict to the user to store onle one type of data rather you have a liberty to hold and kind of data into it. 
