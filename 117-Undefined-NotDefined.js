ecc3992fe97d3a2c985830c0ffa98d0d41694c6c
Believeharsh <bontinue@gmail.com>
2023-12-31 09:37:01 +0530

    added definition of Undefined or Not defined in JS

--- 117-Undefined-NotDefined.js
+++ 117-Undefined-NotDefined.js
@@ -1,5 +1,19 @@
 // 1. What is undefined in js ?
+
+// undefined is a property of the global object. That is, it is a variable in global scope.
+
+// A variable that has not been assigned a value is of type undefined. A method or statement also returns undefined if the variable that is being evaluated does not have an assigned value. A function returns undefined if a value was not returned.
+
+
 // 2. What is the mean of term called Not defined ?
+
+// The JavaScript exception "variable is not defined" occurs when there is a non-existent variable referenced somewhere
+
+
+// ReferenceError: "x" is not defined (V8-based & Firefox)
+// ReferenceError: Can't find variable: x (Safari)
+
+
 
 // console.log(a)
 var a;
