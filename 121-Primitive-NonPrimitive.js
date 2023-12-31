4a2feda94e83dd2986dc9111146f6f0f47714ad1
Believeharsh <bontinue@gmail.com>
2023-12-31 17:11:02 +0530

    added definition

---
+++ 121-Primitive-NonPrimitive.js
@@ -0,0 +1,13 @@
+// Every Variable has a data type that tells what kind of data is being stored in a variable. There are two types of data types in JavaScript.
+
+// Primitive data types 
+// Non-primitive data types
+
+
+// Primitive data types:
+// The predefined data types provided by JavaScript language are known as primitive data types. Primitive data types are also known as in-built data types.
+
+
+
+// Non-primitive data types:
+// The data types that are derived from primitive data types of the JavaScript language are known as non-primitive data types. It is also known as derived data types or reference data types. like arrays and objects 
