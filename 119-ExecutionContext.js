bea2d7e02eae91091d019a0b82bd331e778e994e
Believeharsh <bontinue@gmail.com>
2024-01-02 21:12:11 +0530

    added basic definition

--- 119-ExecutionContext.js
+++ 119-ExecutionContext.js
@@ -1,1 +1,18 @@
 // What is execution context in JS ? 
+// When the JavaScript engine scans a script file, it makes an environment called the Execution Context that handles the entire transformation and execution of the code.
+
+// During the context runtime, the parser parses the source code and allocates memory for the variables and functions. The source code is generated and gets executed.
+
+
+
+// There are two types of execution contexts: global and function. The global execution context is created when a JavaScript script first starts to run, and it represents the global scope in JavaScript. A function execution context is created whenever a function is called, representing the function's local scope
+
+
+
+
+// Phases of the JavaScript Execution Context
+// There are two phases of JavaScript execution context:
+
+// Creation phase: In this phase, the JavaScript engine creates the execution context and sets up the script's environment. It determines the values of variables and functions and sets up the scope chain for the execution context.
+// Execution phase: In this phase, the JavaScript engine executes the code in the execution context. It processes any statements or expressions in the script and evaluates any function calls.
+// Everything in JS happens inside this execution context. It is divided into two components. One is memory and the other is code. It is important to remember that these phases and components are applicable to both global and functional execution contexts.
