14ec3205095d9434af0b593ff0d61183ca04a214
Believeharsh <bontinue@gmail.com>
2024-01-06 18:57:05 +0530

    formaed

--- 121-Variables.js
+++ 121-Variables.js
@@ -1,11 +1,10 @@
 // All three keywords do the basic task of declaring a variable but with some differences Initially, all the variables in JavaScript were written using the var keyword but in ES6 the keywords let and const were introduced.
 
-// Why we dont use var anymore in js code? 
+// Why we dont use var anymore in js code?
 
 // The drawback of using var in react is that it has function scope and it is not block scoped, meaning that it can be accessed anywhere within the function it was declared in. This can lead to unexpected behavior and potential bugs.
 
 // For example, if you declare a variable using var inside a loop, it will be accessible outside the loop as well. This can lead to unexpected behavior if you are not aware of this.
-
 
 // Another drawback of using var is that it can be hoisted to the top of the function, which can lead to unexpected behavior if you are not aware of this.
 
@@ -13,7 +12,7 @@
 
 // Overall, it is generally recommended to use the let and const keywords instead of var when declaring variables in react. This will help to avoid unexpected behavior and potential bugs.
 
-// Why we should use let and const instead of var? 
+// Why we should use let and const instead of var?
 // In JavaScript, the let and const keywords were introduced in ES6 to provide more control over variable scoping and mutability. Here are some reasons why you should use let and const instead of the older var keyword:
 
 // 1. Block Scope:
