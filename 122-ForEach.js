4bcdf34dd0e4f0f14827711cbd7cff79b9e1f99f
Believeharsh <bontinue@gmail.com>
2024-01-07 22:02:33 +0530

    added basic information about forEach method in Js

--- 122-ForEach.js
+++ 122-ForEach.js
@@ -3,3 +3,11 @@
 // The forEach() method is used to iterate over arrays in JavaScript. It takes a callback function as an argument, which is executed for each element of the array. The callback function can be used to perform any operation on the element, such as logging it to the console, modifying it, or adding it to a new array.
 
 
+// The forEach() method is a very useful tool for working with arrays, and it is often used in conjunction with other methods, such as map() and filter().
+
+// Here is an example of how to use the forEach() method:
+// const numbers = [1, 2, 3, 4, 5];
+
+// numbers.forEach(number => {
+//   console.log(number);
+// });
