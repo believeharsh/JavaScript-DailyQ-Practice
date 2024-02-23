7dc0399a98826e4d74b9a521420b71e8e4c6d426
Harsh Dahiya <123488839+believeharsh@users.noreply.github.com>
2024-02-23 23:44:48 +0530

    Update 126-For-In-Loop.js

--- 126-For-In-Loop.js
+++ 126-For-In-Loop.js
@@ -8,7 +8,9 @@
 
 // Important Points
 // Use the for in loop to iterate over non-array objects. Even though we can use a for in loop for an array, it is generally not recommended. Instead, use a for loop for looping over an array.
+
 // The properties iterated with the for in loop also include the properties of the objects higher in the Prototype chain.
+
 // The order in which properties are iterated may not match the properties that are defined in the object.
 
 
